328c4be6d2b0935914f614441b132133ffe71e27
Gregor Dschung <gregor@chkpnt.de>
2018-07-28 12:21:35 +0200

    Fix tests on Windows

--- ChkpntApi.Tests/Webhooks/WebhookEventHandlerTests.cs
+++ ChkpntApi.Tests/Webhooks/WebhookEventHandlerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Abstractions.TestingHelpers;
+using System.Runtime.InteropServices;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
@@ -21,21 +22,41 @@ namespace ChkpntApi.Webhooks.Tests
             _processRunner = new Mock<IProcessRunner>();
         }
 
-        [Fact]
-        public void FileDoesNotExist()
+        [SkippableFact]
+        public void FileDoesNotExist_Linux()
         {
+            Skip.IfNot(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                    || RuntimeInformation.IsOSPlatform(OSPlatform.OSX));
+            
             _filesystem.AddFile("./handlers/repo1/push", new MockFileData("") );
             var options = Options.Create(new WebhooksSettings { HandlersPath = @"./handlers" });
             var sut = new WebhookEventHandler(options, _filesystem, _processRunner.Object);
-            
+
             sut.Consume(@event: "does-not-exist", id: "repo1");
 
             _processRunner.Verify(p => p.Start(It.IsAny<string>()), Times.Never());
         }
 
-        [Fact]
-        public void FileExists()
+        [SkippableFact]
+        public void FileDoesNotExist_Windows()
         {
+            Skip.IfNot(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+            
+            _filesystem.AddFile(@".\handlers\repo1\push", new MockFileData("") );
+            var options = Options.Create(new WebhooksSettings { HandlersPath = @".\handlers" });
+            var sut = new WebhookEventHandler(options, _filesystem, _processRunner.Object);
+
+            sut.Consume(@event: "does-not-exist", id: "repo1");
+
+            _processRunner.Verify(p => p.Start(It.IsAny<string>()), Times.Never());
+        }
+
+        [SkippableFact]
+        public void FileExists_Linux()
+        {
+            Skip.IfNot(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                    || RuntimeInformation.IsOSPlatform(OSPlatform.OSX));
+
             _filesystem.AddFile("./handlers/repo1/push", new MockFileData("") );
             var options = Options.Create(new WebhooksSettings { HandlersPath = @"./handlers" });
             var sut = new WebhookEventHandler(options, _filesystem, _processRunner.Object);
@@ -43,6 +64,20 @@ namespace ChkpntApi.Webhooks.Tests
             sut.Consume(@event: "push", id: "repo1");
 
             _processRunner.Verify(p => p.Start("./handlers/repo1/push"));
+        }
+
+        [SkippableFact]
+        public void FileExists_Windows()
+        {
+            Skip.IfNot(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+
+            _filesystem.AddFile(@".\handlers\repo1\push", new MockFileData("") );
+            var options = Options.Create(new WebhooksSettings { HandlersPath = @".\handlers" });
+            var sut = new WebhookEventHandler(options, _filesystem, _processRunner.Object);
+
+            sut.Consume(@event: "push", id: "repo1");
+
+            _processRunner.Verify(p => p.Start(@".\handlers\repo1\push"));
         }
     }
 }
